bb764843643c272ec534735be07d954ed8b483c5
salsoftmobile <mobile.vcs123@gmail.com>
2021-03-30 16:37:26 +0500

    navigation bug fixed

--- src/screens/SettingsScreen/index.js
+++ src/screens/SettingsScreen/index.js
@@ -1,49 +1,69 @@
 import React from 'react';
-import {Text, View, Image,TouchableOpacity} from 'react-native';
-import { ScrollView } from 'react-native-gesture-handler';
+import {Text, View, Image, TouchableOpacity} from 'react-native';
+import {ScrollView} from 'react-native-gesture-handler';
 import NotificationHeader from '../../components/NotificationHeader/index';
 import styles from './styles';
 
-function SettingsScreen({navigation}){
+function SettingsScreen({navigation}) {
+  const renderQuizBar = () => {
+    return (
+      <ScrollView>
+        <TouchableOpacity
+          onPress={() => navigation.replace('MyTab')}
+          style={styles.touchableRow}>
+          <View style={styles.rowView}>
+            <Image
+              style={styles.rowIconSize3}
+              source={require('../../assets/images/quizIcons/about.png')}
+            />
+            <Text style={styles.rowText}>HOME</Text>
+          </View>
+          <Image
+            style={styles.rowIcon}
+            source={require('../../assets/images/quizIcons/greater.png')}
+          />
+        </TouchableOpacity>
 
-    const renderQuizBar = () => {
-        return(
-            <ScrollView>
-            <TouchableOpacity onPress={()=> navigation.navigate('AllQuiz')} style={styles.touchableRow}>
-                <View style={styles.rowView}>
-                <Image style={styles.rowIconSize3} source={require('../../assets/images/quizIcons/about.png')}/>
-                <Text style={styles.rowText}>TAKE QUIZ</Text>
-                </View>
-                <Image style={styles.rowIcon} source={require('../../assets/images/quizIcons/greater.png')}/>
-            </TouchableOpacity>
+        <TouchableOpacity
+          onPress={() => navigation.navigate('AllQuiz')}
+          style={styles.touchableRow}>
+          <View style={styles.rowView}>
+            <Image
+              style={styles.rowIconSize3}
+              source={require('../../assets/images/quizIcons/about.png')}
+            />
+            <Text style={styles.rowText}>TAKE QUIZ</Text>
+          </View>
+          <Image
+            style={styles.rowIcon}
+            source={require('../../assets/images/quizIcons/greater.png')}
+          />
+        </TouchableOpacity>
 
-            <TouchableOpacity onPress={()=> navigation.navigate('Notification')} style={styles.touchableRow}>
-                <View style={styles.rowView}>
-                <Image style={styles.rowIconSize3} source={require('../../assets/images/quizIcons/about.png')}/>
-                <Text style={styles.rowText}>MY HISTORY</Text>
-                </View>
-                <Image style={styles.rowIcon} source={require('../../assets/images/quizIcons/greater.png')}/>
-            </TouchableOpacity>
-
-            <TouchableOpacity onPress={()=> navigation.navigate('About')} style={styles.touchableRow}>
-                <View style={styles.rowView}>
-                <Image style={styles.rowIconSize3} source={require('../../assets/images/quizIcons/about.png')}/>
-                <Text style={styles.rowText}>ABOUT</Text>
-                </View>
-                <Image style={styles.rowIcon} source={require('../../assets/images/quizIcons/greater.png')}/>
-            </TouchableOpacity>
-
-            </ScrollView>
-            
-        )
-    }
-    return(
-        <View style={styles.mainSettingsScreen}>
-        <NotificationHeader title='Settings'/>
-        {renderQuizBar()}
-            
-        </View>
-    )
+        <TouchableOpacity
+          onPress={() => navigation.navigate('About')}
+          style={styles.touchableRow}>
+          <View style={styles.rowView}>
+            <Image
+              style={styles.rowIconSize3}
+              source={require('../../assets/images/quizIcons/about.png')}
+            />
+            <Text style={styles.rowText}>ABOUT</Text>
+          </View>
+          <Image
+            style={styles.rowIcon}
+            source={require('../../assets/images/quizIcons/greater.png')}
+          />
+        </TouchableOpacity>
+      </ScrollView>
+    );
+  };
+  return (
+    <View style={styles.mainSettingsScreen}>
+      <NotificationHeader title="Settings" />
+      {renderQuizBar()}
+    </View>
+  );
 }
 
-export default SettingsScreen
+export default SettingsScreen;
